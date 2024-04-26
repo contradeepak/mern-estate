47ab5866d299651088f0ac42707a4f2ebf31370b
contradeepak <deepaktomss@gmail.com>
2024-04-26 21:02:56 +0530

    add image slider to the listing page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
   server: {
     proxy: {
       '/api': {
@@ -11,6 +15,7 @@ export default defineConfig({
       },
     },
   },
+
 
   plugins: [react()],
 });
