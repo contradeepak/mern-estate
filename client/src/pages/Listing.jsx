c77a119ac6c921a8120edcd33b9780ba19824d2e
contradeepak <deepaktomss@gmail.com>
2024-04-29 11:43:19 +0530

    Create the listing item components and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -26,7 +26,7 @@ export default function Listing() {
   const [copied, setCopied] = useState(false);
   const [contact, setContact] = useState(false);
   const params = useParams();
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -49,7 +49,6 @@ export default function Listing() {
     };
     fetchListing();
   }, [params.listingId]);
- 
 
   return (
     <main>
@@ -107,7 +106,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
             </div>
@@ -137,13 +136,15 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact
-            && (
-            <button onClick={()=>setContact(true)}
-            className='bg-slate-700 text-white
-            rounded-lg uppercase hover:opacity-95 p-3'>Contact Landlord</button>   
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
             )}
-            {contact && <Contact listing={listing}/>}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
